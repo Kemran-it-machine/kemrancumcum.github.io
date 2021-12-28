34963175f90fd0697a43b9d4dcb2da8e9b33e4dc
Kemran-it-machine <90641770+Kemran-it-machine@users.noreply.github.com>
2021-12-29 02:10:50 +0300

    Update js5.js

--- js5.js
+++ js5.js
@@ -1,32 +1,13 @@
-function click1() {
+function calc() {
     let price = document.getElementsByName("price");
-    let count = document.getElementsByName("count");
-    let result = document.getElementById("result");
-    let toPrice = price[0].value;
-    let toCount = count[0].value;
-    let dag = toCount.match(/^\d+$/);
-    let che = toPrice.match(/^\d+$/);
-    if(che !== null && dag !== null) {
-    result.innerHTML = parseInt(price[0].value) * parseInt(count[0].value);
+    let quan = document.getElementsByName("quantity");
+    if ((/^[1-9][0-9]*$/).test(price[0].value) && (/^[1-9][0-9]*$/).test(quan[0].value)) {
+        alert("Итоговая цена = " + parseInt(price[0].value) * parseInt(quan[0].value));
+    } else {
+        alert("Введите корректные данные!");
     }
-    else if(che === null && dag !== null ){
-    alert("Введите число в поле - цена !");
-    }
-    else if(dag === null && che !== null) {
-      alert("Введите число в поле - количество !");
-    }
-    else if(che === null && dag === null) {
-      alert("Вводите числа в оба поля !");
-    }
-    return false;
-  }
-
- function onClick() {
-    alert("Стоимость найдена");
-  }
-  
-  window.addEventListener("DOMContentLoaded", function (event) {
-    console.log("DOM fully loaded and parsed");
-    let b = document.getElementById("calculateResult");
-    b.addEventListener("click", onClick);
-  });
+}
+window.addEventListener("DOMContentLoaded", function(event) {
+    let b = document.getElementById("result");
+    b.addEventListener("click", calc);
+});
